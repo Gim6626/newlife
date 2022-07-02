bd8d0546bff91cf4709143fe54e19f50b7849896
Dmitriy Vinokurov <gim6626@gmail.com>
2022-07-02 16:08:18 +0400

    Gim6626/newlife#2 Evolution strategy with mutations

--- newlifelib/graphics.py
+++ newlifelib/graphics.py
@@ -1,9 +1,15 @@
 from enum import Enum
 
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtGui import (
+    QPainter,
+    QBrush,
+)
 from PyQt5 import QtGui
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import (
+    Qt,
+    QPoint,
+)
 
 from newlifelib.logic import *
 
@@ -17,12 +23,10 @@ class LifeWindow(QMainWindow):
 
     def __init__(self,
                  args,
-                 cell_size: int,
-                 color_mode: ColorMode,
                  logger):
         super().__init__()
-        self.cell_size = cell_size
-        self.color_mode = color_mode
+        self.cell_size = args.cell_size
+        self.color_mode = args.color_mode
         if args.fullscreen or args.maximized:
             if args.use_primary_screen:
                 geometry = QApplication.instance().primaryScreen().availableGeometry()
@@ -33,7 +37,8 @@ class LifeWindow(QMainWindow):
         else:
             grid_width = args.width
             grid_height = args.height
-        self.life_grid = LifeGrid(grid_width,
+        self.life_grid = LifeGrid(args.evolution_strategy_class,
+                                  grid_width,
                                   grid_height,
                                   args.birth_probability,
                                   args.click_birth_probability,
@@ -69,37 +74,101 @@ class LifeWindow(QMainWindow):
     def paintEvent(self, event):
         painter = QPainter()
         painter.begin(self)
+        painter.setBrush(QBrush(Qt.black))
+        painter.drawRect(0,
+                         0,
+                         self.life_grid.height * self.cell_size,
+                         self.life_grid.width * self.cell_size)
         for iv in range(self.life_grid.height):
             for ih in range(self.life_grid.width):
-                state = self.life_grid.cells[iv][ih].state
+                current_cell = self.life_grid.cells[iv][ih]
+                if current_cell is None:
+                    continue
+                state = current_cell.state
+
                 if self.color_mode == ColorMode.MONOCHROME:
-                    if state in (CellState.NEWBORN,
-                                 CellState.GROWN,
-                                 CellState.MATURE,
-                                 CellState.LONG_LIVING):
-                        color = Qt.white
-                    elif state in (CellState.NONE,
-                                   CellState.DYING):
-                        color = Qt.black
+                    if isinstance(self.life_grid.evolution_strategy, ClassicEvolutionStrategy):
+                        if state in (CellState.NEWBORN,
+                                     CellState.GROWN,
+                                     CellState.MATURE,
+                                     CellState.LONG_LIVING):
+                            primary_color = Qt.white
+                            secondary_color = Qt.gray
+                        elif state == CellState.DYING:
+                            primary_color = Qt.black
+                            secondary_color = Qt.black
+                        else:
+                            raise NotImplementedError
+                    elif isinstance(self.life_grid.evolution_strategy, MutationEvolutionStrategy):
+                        # TODO: Implement
+                        raise NotImplementedError
                     else:
                         raise NotImplementedError
                 elif self.color_mode == ColorMode.COLOR:
-                    if state == CellState.NEWBORN:
-                        color = Qt.green
-                    elif state == CellState.GROWN:
-                        color = Qt.blue
-                    elif state == CellState.MATURE:
-                        color = Qt.cyan
-                    elif state == CellState.LONG_LIVING:
-                        color = Qt.yellow
-                    elif state == CellState.DYING:
-                        color = Qt.red
-                    elif state == CellState.NONE:
-                        color = Qt.black
+                    if isinstance(self.life_grid.evolution_strategy, ClassicEvolutionStrategy):
+                        if state == CellState.NEWBORN:
+                            primary_color = Qt.green
+                            secondary_color = Qt.white
+                        elif state == CellState.GROWN:
+                            primary_color = Qt.blue
+                            secondary_color = Qt.white
+                        elif state == CellState.MATURE:
+                            primary_color = Qt.cyan
+                            secondary_color = Qt.white
+                        elif state == CellState.LONG_LIVING:
+                            primary_color = Qt.yellow
+                            secondary_color = Qt.white
+                        elif state == CellState.DYING:
+                            primary_color = Qt.red
+                            secondary_color = Qt.white
+                        else:
+                            raise NotImplementedError
+                    elif isinstance(self.life_grid.evolution_strategy, MutationEvolutionStrategy):
+                        if state == CellState.NEWBORN:
+                            secondary_color = Qt.green
+                        elif state == CellState.GROWN:
+                            secondary_color = Qt.blue
+                        elif state == CellState.MATURE:
+                            secondary_color = Qt.cyan
+                        elif state == CellState.LONG_LIVING:
+                            secondary_color = Qt.yellow
+                        elif state == CellState.DYING:
+                            secondary_color = Qt.red
+                        else:
+                            raise NotImplementedError
+                        if isinstance(current_cell, TypicalCell):
+                            primary_color = Qt.gray
+                        elif isinstance(current_cell, StandaloneCell):
+                            primary_color = Qt.darkMagenta
+                        elif isinstance(current_cell, SociableCell):
+                            primary_color = Qt.darkCyan
+                        elif isinstance(current_cell, StandaloneAndSociableCell):
+                            primary_color = Qt.darkYellow
+                        elif isinstance(current_cell, SuperStandaloneCell):
+                            primary_color = Qt.darkBlue
+                        elif isinstance(current_cell, SuperSociableCell):
+                            primary_color = Qt.darkGreen
+                        elif isinstance(current_cell, SuperStandaloneAndSociableCell):
+                            primary_color = Qt.white
+                        else:
+                            raise NotImplementedError
                     else:
                         raise NotImplementedError
                 else:
                     raise NotImplementedError
-                painter.setPen(QPen(color, self.cell_size))
-                painter.drawPoint(ih * self.cell_size, iv * self.cell_size)
+
+                if isinstance(self.life_grid.evolution_strategy, ClassicEvolutionStrategy) \
+                        or isinstance(self.life_grid.evolution_strategy, MutationEvolutionStrategy):
+                    painter.setBrush(QBrush(secondary_color))
+                    painter.drawEllipse(QPoint(ih * self.cell_size - math.ceil(self.cell_size / 2),
+                                               iv * self.cell_size - math.ceil(self.cell_size / 2)),
+                                        math.ceil(self.cell_size / 2) - 1,
+                                        math.ceil(self.cell_size / 2) - 1)
+                    painter.setBrush(QBrush(primary_color))
+                    painter.drawEllipse(QPoint(ih * self.cell_size - math.ceil(self.cell_size / 2),
+                                               iv * self.cell_size - math.ceil(self.cell_size / 2)),
+                                        math.ceil(self.cell_size / 4),
+                                        math.ceil(self.cell_size / 4))
+                else:
+                    raise NotImplementedError
         painter.end()
--- newlifelib/logic.py
+++ newlifelib/logic.py
@@ -1,6 +1,11 @@
 import random
 from enum import Enum
 import math
+from abc import abstractmethod
+import random
+from typing import (
+    List
+)
 
 from .util import Logger
 
@@ -11,95 +16,199 @@ class CellState(Enum):
     MATURE = 'mature'
     LONG_LIVING = 'longliving'
     DYING = 'dying'
-    NONE = 'none'
 
 
-class Cell:
+class BasicEvolutionStrategy:
+
+    def __init__(self,
+                 grid: 'LifeGrid'):
+        self.grid = grid
+
+    @abstractmethod
+    def born_new_cell(self,
+                      coord_vertical: int,
+                      coord_horizontal: int):
+        pass
+
+    @abstractmethod
+    def if_will_born(self,
+                     coord_vertical: int,
+                     coord_horizontal: int):
+        pass
+
+
+class ClassicEvolutionStrategy(BasicEvolutionStrategy):
+
+    def born_new_cell(self,
+                      coord_vertical: int,
+                      coord_horizontal: int):
+        return TypicalCell(self.grid, coord_vertical, coord_horizontal)
+
+    def if_will_born(self,
+                     coord_vertical: int,
+                     coord_horizontal: int):
+        alive_neighbours = self.grid.alive_neighbours(coord_vertical, coord_horizontal)
+        if TypicalCell.will_survive_with_neighbours(alive_neighbours):
+            return TypicalCell
+        else:
+            return None
+
+
+class MutationEvolutionStrategy(BasicEvolutionStrategy):
+
+    MAX_AGE = 30
+
+    @property
+    def _random_cell_class(self):
+        random_int = random.randint(0, 16)
+        if random_int in (0, 1):
+            cell_class = StandaloneCell
+        elif random_int == 2:
+            cell_class = SuperStandaloneCell
+        elif random_int in (3, 4):
+            cell_class = SociableCell
+        elif random_int == 5:
+            cell_class = SuperSociableCell
+        elif random_int in (6, 7):
+            cell_class = StandaloneAndSociableCell
+        elif random_int == 8:
+            cell_class = SuperStandaloneAndSociableCell
+        else:
+            cell_class = TypicalCell
+        return cell_class
+
+    def born_new_cell(self,
+                      coord_vertical: int,
+                      coord_horizontal: int):
+        return self._random_cell_class(self.grid,
+                                       coord_vertical,
+                                       coord_horizontal)
+
+    def if_will_born(self,
+                     coord_vertical: int,
+                     coord_horizontal: int):
+        alive_neighbours = self.grid.alive_neighbours(coord_vertical, coord_horizontal)
+        cell_class = self._random_cell_class
+        if cell_class.will_survive_with_neighbours(alive_neighbours):
+            return cell_class
+        else:
+            return None
+
+
+class BasicCell:
+
     def __init__(self,
                  grid: 'LifeGrid',
                  coord_vertical: int,
-                 coord_horizontal: int,
-                 alive: bool = None,
-                 age: int = None):
+                 coord_horizontal: int):
         self.grid = grid
         self.logger = self.grid.logger
         self.coord_vertical = coord_vertical
         self.coord_horizontal = coord_horizontal
-        self.alive = alive
-        self.alive_prev = None
-        self.alive_next = None
-        self.age = age
+        self.will_die = False
+        self.age = 0
 
     @property
     def state(self) -> CellState:
-        if not self.alive_prev and self.alive:
+        if self.age == 0:
             return CellState.NEWBORN
-        elif self.alive_prev and self.alive:
+        elif self.age > 0 and not self.will_die:
             if self.age < 5:
                 return CellState.GROWN
-            elif 5 <= self.age < 50:
+            elif 5 <= self.age < 15:
                 return CellState.MATURE
             else:
                 return CellState.LONG_LIVING
-        elif not self.alive and self.alive_prev:
+        elif self.will_die:
             return CellState.DYING
         else:
-            return CellState.NONE
+            raise NotImplementedError
 
-    def next_generation(self):
-        self.alive_prev = self.alive
-        self.alive = self.alive_next
-        self.alive_next = None
+    @classmethod
+    def will_survive_with_neighbours(cls, alive_neighbours):
+        pass
 
-    def kill(self):
-        self.alive_next = False
-        self.age = 0
-
-    def give_birth(self):
-        self.alive_next = True
-        self.age = 0
-
-    def live_more(self):
-        self.alive_next = True
-        self.age += 1
+    @property
+    def will_survive(self):
+        return self.will_survive_with_neighbours(self.alive_neighbours)
 
     @property
     def alive_neighbours(self):
-        iv_from = self.coord_vertical - 1 if self.coord_vertical > 0 else 0
-        iv_to = self.coord_vertical + 1 if self.coord_vertical < self.grid.height - 1 else self.grid.height - 1
-        ih_from = self.coord_horizontal - 1 if self.coord_horizontal > 0 else 0
-        ih_to = self.coord_horizontal + 1 if self.coord_horizontal < self.grid.width - 1 else self.grid.width - 1
-        alive_neighbours = 0
-        for iv in range(iv_from, iv_to + 1):
-            for ih in range(ih_from, ih_to + 1):
-                if iv == self.coord_vertical and ih == self.coord_horizontal:
-                    continue
-                if self.grid.cells[iv][ih].alive:
-                    alive_neighbours += 1
-        return alive_neighbours
+        return self.grid.alive_neighbours(self.coord_vertical, self.coord_horizontal)
+
+
+class TypicalCell(BasicCell):
+
+    @classmethod
+    def will_survive_with_neighbours(cls, alive_neighbours):
+        return alive_neighbours in (2, 3)
+
+
+class StandaloneCell(BasicCell):
+
+    @classmethod
+    def will_survive_with_neighbours(cls, alive_neighbours):
+        return alive_neighbours in (1, 2, 3)
+
+
+class SuperStandaloneCell(BasicCell):
+
+    @classmethod
+    def will_survive_with_neighbours(cls, alive_neighbours):
+        return alive_neighbours in (0, 1, 2, 3)
+
+
+class SociableCell(BasicCell):
+
+    @classmethod
+    def will_survive_with_neighbours(cls, alive_neighbours):
+        return alive_neighbours in (2, 3, 4)
+
+
+class SuperSociableCell(BasicCell):
+
+    @classmethod
+    def will_survive_with_neighbours(cls, alive_neighbours):
+        return alive_neighbours in (2, 3, 4, 5)
+
+
+class StandaloneAndSociableCell(BasicCell):
+
+    @classmethod
+    def will_survive_with_neighbours(cls, alive_neighbours):
+        return alive_neighbours in (1, 2, 3, 4)
+
+
+class SuperStandaloneAndSociableCell(BasicCell):
+
+    @classmethod
+    def will_survive_with_neighbours(cls, alive_neighbours):
+        return alive_neighbours in (0, 1, 2, 3, 4, 5)
 
 
 class LifeGrid:
 
     def __init__(self,
+                 evolution_strategy_class,
                  width: int,
                  height: int,
                  birth_probability: float,
                  click_birth_probability: float,
                  click_birth_radius: int,
                  logger: Logger):
+        self.evolution_strategy = evolution_strategy_class(self)
         self.logger = logger
         self.width = width
         self.height = height
         self.birth_probability = birth_probability
         self.click_birth_probability = click_birth_probability
         self.click_birth_radius = click_birth_radius
-        self.cells = []
+        self.cells: List[List[BasicCell]] = []
         self.generations_count = 0
         for iv in range(self.height):
             row = []
             for ih in range(self.width):
-                row.append(Cell(self, iv, ih, False, 0))
+                row.append(None)
             self.cells.append(row)
 
     def fill_random(self):
@@ -108,10 +217,8 @@ class LifeGrid:
             for ih in range(self.width):
                 rnd = random.random()
                 if rnd < self.birth_probability:
-                    self.cells[iv][ih].alive = True
                     born_count += 1
-                else:
-                    self.cells[iv][ih].alive = False
+                    self.cells[iv][ih] = self.evolution_strategy.born_new_cell(iv, ih)
         self.logger.info(f'{born_count} cell(s) randomly born')
         self.generations_count += 1
 
@@ -129,11 +236,24 @@ class LifeGrid:
                     continue
                 rnd = random.random()
                 if rnd < self.click_birth_probability:
-                    self.cells[iv][ih].alive = True
+                    if self.cells[iv][ih] is not None:
+                        self.cells[iv][ih] = self.evolution_strategy.born_new_cell(self, iv, ih)
                     born_count += 1
-                else:
-                    self.cells[iv][ih].alive = False
         self.logger.info(f'{born_count} cell(s) randomly born by command')
+
+    def alive_neighbours(self, coord_vertical, coord_horizontal):
+        iv_from = coord_vertical - 1 if coord_vertical > 0 else 0
+        iv_to = coord_vertical + 1 if coord_vertical < self.height - 1 else self.height - 1
+        ih_from = coord_horizontal - 1 if coord_horizontal > 0 else 0
+        ih_to = coord_horizontal + 1 if coord_horizontal < self.width - 1 else self.width - 1
+        alive_neighbours = 0
+        for iv in range(iv_from, iv_to + 1):
+            for ih in range(ih_from, ih_to + 1):
+                if iv == coord_vertical and ih == coord_horizontal:
+                    continue
+                if self.cells[iv][ih] is not None:
+                    alive_neighbours += 1
+        return alive_neighbours
 
     def next_generation(self):
         total_alive_count = 0
@@ -141,33 +261,37 @@ class LifeGrid:
         died_count = 0
         born_count = 0
         total_ages = 0
+        future_cells = []
         for iv in range(self.height):
+            future_cells_row = []
             for ih in range(self.width):
                 current_cell = self.cells[iv][ih]
-                alive_neighbours = current_cell.alive_neighbours
-                alive_next = True if alive_neighbours in (2, 3) else False
-                if current_cell.alive and alive_next:
-                    current_cell.live_more()
-                    total_alive_count += 1
-                    survived_count += 1
-                    total_ages += current_cell.age
-                elif current_cell.alive and not alive_next:
-                    current_cell.kill()
-                    died_count += 1
-                elif not current_cell.alive and alive_next:
-                    current_cell.give_birth()
-                    total_alive_count += 1
-                    born_count += 1
-                    total_ages += 1
-                elif not current_cell.alive and not alive_next:
-                    pass
+                will_survive = current_cell.will_survive if current_cell is not None else False
+                will_born_cell_class = self.evolution_strategy.if_will_born(iv, ih)
+                if current_cell is not None:
+                    if isinstance(self.evolution_strategy, MutationEvolutionStrategy) and current_cell.age > MutationEvolutionStrategy.MAX_AGE:
+                        will_survive = False
+                    if will_survive:
+                        future_cell = current_cell
+                        current_cell.age += 1
+                        total_alive_count += 1
+                        survived_count += 1
+                        total_ages += current_cell.age
+                    else:
+                        future_cell = None
+                        died_count += 1
                 else:
-                    self.logger.critical(f'Invalid alive ({current_cell.alive}) and alive next ({alive_next}) combination for cell [{iv}, {ih}]')
-                    raise NotImplementedError
-        for iv in range(self.height):
-            for ih in range(self.width):
-                self.cells[iv][ih].next_generation()
+                    if will_born_cell_class is not None:
+                        future_cell = will_born_cell_class(self, iv, ih)
+                        total_alive_count += 1
+                        born_count += 1
+                        total_ages += 1
+                    else:
+                        future_cell = None
+                future_cells_row.append(future_cell)
+            future_cells.append(future_cells_row)
+        self.cells = future_cells
         average_age = total_ages / total_alive_count if total_alive_count > 0 else 0
-        alive_percent = total_alive_count / (self.width * self.height)
+        alive_percent = total_alive_count / (self.width * self.height) * 100
         self.logger.info(f'Generation #{self.generations_count}: {born_count} born, {survived_count} survived, {died_count} died, {total_alive_count}/{self.width * self.height} ({alive_percent:.2f}%) total alive, {average_age:.3f} average age')
         self.generations_count += 1
